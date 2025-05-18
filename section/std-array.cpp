dd1bdde26f8bc7d43ad545189e4802ab5fa535ad
intelli.hill <intelli.hill@outlook.com>
2025-05-18 17:55:06 +0900

    hong: vector

---
+++ section/std-array.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <array>
+
+using namespace std;
+
+int main() {
+    int array[5] = {1, 2, 3, 4, 5};
+
+    std::array<int, 5> my_arr = {1, 2, 3, 4, 5};
+
+    return 0;
+}
