13cc5629c4444914316ba7126a08dc37aa88aef4
intelli.hill <intelli.hill@outlook.com>
2025-06-18 20:58:50 +0900

    make vector in xcode

---
+++ modern_cpp/modern_cpp/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  modern_cpp
+//
+//  Created by 정대로 on 6/18/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
