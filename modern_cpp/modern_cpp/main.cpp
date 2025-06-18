605b3aeb86e2681700e32d7591c2db416fb16a2d
intelli.hill <intelli.hill@outlook.com>
2025-06-18 21:03:43 +0900

    make vector in xcode

--- modern_cpp/modern_cpp/main.cpp
+++ modern_cpp/modern_cpp/main.cpp
@@ -1,14 +1,40 @@
-//
-//  main.cpp
-//  modern_cpp
-//
-//  Created by 정대로 on 6/18/25.
-//
-
 #include <iostream>
+#include <cassert>
+
+class vector {
+public:
+    vector(int size) : my_size(size), data(new double[size]) {
+        std::cout << "Constructor: " << my_size << std::endl;
+    }
+    
+    ~vector() {
+        std::cout << "DeConstructor: " << std::endl;
+        delete [] data;
+    }
+    
+    void operator=(const vector& that) {
+        assert(that.my_size == my_size);
+        
+        for (int i=0; i<my_size; ++i) {
+            data[i] = that.data[i];
+        }
+    }
+    
+    double& operator[](int i) const {
+        assert(i>=0 && i<my_size);
+        return data[i];
+    }
+    
+private:
+    int my_size;
+    double* data;
+};
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    vector v1(4);
+    v1[0] = v1[1] = 1.0;
+    
+    std::cout << "test" << std::endl;
+    
     return 0;
 }
