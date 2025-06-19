28742eff89edd923cb375eb99042dc2170a2ca3c
intelli.hill <intelli.hill@outlook.com>
2025-06-20 07:37:38 +0900

    leet: 1672

--- leetcode/1672_Richest_Customer_Wealth.cpp
+++ leetcode/1672_Richest_Customer_Wealth.cpp
@@ -3,18 +3,37 @@
 
 using namespace std;
 
-// class Solution {
-// public:
-//     int maximumWealth(vector<vector<int>>& accounts) {
-//         return 0
-//     }
-
-// }
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) {
+        for (const auto row : accounts) {
+            for (int val : row) {
+                nums += val;
+            }
+            cout << "max: " << max << "nums: " << nums << endl;
+            if (nums >= max) {
+                max= nums;
+            }
+            nums= 0;
+        }
+        cout << max << endl;
+        return 0;
+    }
+private:
+    int nums= 0;
+    int max= 0;
+};
 
 int main() {
 
-    std::vector<vector<int>> array = {1,2,3}{4,5,6};
-    cout << array.size() << endl;
+    std::vector<vector<int>> array = {
+        {1, 2},
+        {3, 4},
+        {2, 3}
+    };
+
+    Solution solution;
+    solution.maximumWealth(array);
 
     return 0;
 }
