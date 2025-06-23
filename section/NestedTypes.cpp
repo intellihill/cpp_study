9373361e5cb778edbfe88e54ef6ab564993df7c6
intelli.hill <intelli.hill@outlook.com>
2025-06-24 05:37:48 +0900

    NestedType

---
+++ section/NestedTypes.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+
+
+class Fruit {
+public:
+    enum FruitType {
+        APPLE, BANANA, CHERRY,
+    };
+
+private:
+    FruitType m_type;
+    
+public:
+
+    Fruit(FruitType type) : m_type(type) {}
+
+    FruitType getType() {
+        return m_type;
+    }
+};
+
+int main() {
+    Fruit apple(Fruit::FruitType::APPLE);
+
+    if (apple.getType() == Fruit::FruitType::APPLE) {
+        std::cout << "Apple" << std::endl;
+    }
+
+    return 0;
+}
