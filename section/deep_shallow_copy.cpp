f31d11c69412434a0fcff05896bf8bb137ac91f3
intelli.hill <intelli.hill@outlook.com>
2025-07-01 21:40:54 +0900

    deep_shallow_copy

---
+++ section/deep_shallow_copy.cpp
@@ -0,0 +1,53 @@
+#include <cassert>
+#include <iostream>
+using namespace std;
+
+class MyString {
+    //private:
+public:
+    char *m_data = nullptr;
+    int m_length = 0;
+
+public:
+    MyString(const char *source = "") {
+        assert(source);
+
+        m_length = std::strlen(source) + 1; // String 의 마지막이라는 '\0'을 넣어주기위해서 +1을 했음
+        m_data = new char[m_length];
+
+        for (int i = 0; i < m_length; ++i) {
+            m_data[i] = source[i];
+        }
+
+        m_data[m_length - 1] = '\0';
+    }
+
+    ~MyString() {
+        delete[] m_data;
+    }
+
+    char* getString() {
+        return m_data;
+    }
+    int getLength() {
+        return m_length;
+    }
+};
+
+int main() {
+    MyString hello("Hello");
+
+    cout << (int*)hello.m_data << endl;
+    cout << hello.getString() << endl;
+
+    {
+        // (기본)복사생성자가 호출된다.
+        MyString copy = hello;
+        cout << (int*)copy.m_data << endl;
+        cout << copy.getString() << endl;
+    }
+
+    cout << hello.getString() << endl;
+
+    return 0;
+}
