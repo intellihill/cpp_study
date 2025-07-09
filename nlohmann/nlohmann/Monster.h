4c713ece92f07b40c2d64fb25dc5907b8ae4e94c
intelli.hill <intelli.hill@outlook.com>
2025-07-09 21:54:31 +0900

    monster 2D

---
+++ nlohmann/nlohmann/Monster.h
@@ -0,0 +1,28 @@
+//
+//  Monster.h
+//  nlohmann
+//
+//  Created by 정대로 on 7/9/25.
+//
+
+#include <string>
+#include "Position2D.h"
+
+class Monster {
+private:
+    std::string m_name;
+    Position2D m_location;
+    
+public:
+    Monster(const std::string name_in, const Position2D & pos_in) : m_name(name_in), m_location(pos_in) {}
+    
+    void moveTo(Position2D & pos_target) {
+        m_location.set(pos_target);
+    }
+    
+    friend std::ostream & operator << (std::ostream & out, const Monster & monster) {
+        out << monster.m_name << " " << monster.m_location << std::endl;
+        return out;
+    }
+
+};
--- nlohmann/nlohmann/main.cpp
+++ nlohmann/nlohmann/main.cpp
@@ -7,16 +7,24 @@
 
 #include <iostream>
 #include <nlohmann/json.hpp>
+#include "Monster.h"
 
 using json = nlohmann::json;
 
 int main(int argc, const char * argv[]) {
 
-    json j;
-    j["name"] = "John";
-    j["age"] = 30;
-    j.size();
-    std::cout << j.dump(4) << std::endl;
+//    json j;
+//    j["name"] = "John";
+//    j["age"] = 30;
+//    j.size();
+//    std::cout << j.dump(4) << std::endl;
+    Monster mon1("Sanson",Position2D(0, 0));
+    
+//    while(1)
+    {
+        mon1.moveTo(Position2D(1, 1));
+        std::cout << mon1 << std::endl;
+    }
     
     return 0;
 }
