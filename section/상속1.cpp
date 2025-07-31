c97c60dc6d0c2bf7e25021131a536ecec041a383
intelli.hill <intelli.hill@outlook.com>
2025-07-31 20:36:13 +0900

    is-a

--- section/상속1.cpp
+++ section/상속1.cpp
@@ -3,12 +3,14 @@ using namespace std;
 
 class Mother {
 private:
+// protected:
     int m_i;
 
 public:
-    // Mother(const int & i_in): m_i(m_in) {
-    //     std::cout << "Mother constructor" << std::endl;
-    // }
+    Mother(): m_i(0) {}
+    Mother(const int & i_in): m_i(0) {
+        std::cout << "Mother constructor" << std::endl;
+    }
 
     void setValue(const int& i_in) {
         m_i= i_in;
@@ -18,7 +20,28 @@ public:
     }
 };
 
-class Child : public Mother {
+class Child : public Mother { // derived class
+private:
+    double m_d;
+
+public:
+    Child(const int & i_in, const double & d_in): Mother(i_in), m_d(d_in) {}
+
+    void setvalue(const int & i_in, const double & d_in) {
+        Mother::setValue(i_in);
+        m_d = d_in;
+    }
+    void setValue(const double & d_in) {
+        m_d = d_in;
+    }
+    double getValue() {
+        return m_d;
+    }
+};
+class Daughter : public Mother {
+
+};
+class Son : public Mother {
 
 };
 
@@ -27,9 +50,12 @@ int main() {
     mother.setValue(1024);
     cout << mother.getValue() << endl;
 
-    Child child;
-    child.setValue(125);
+    // Child child;
+    // child.setValue(125);
 
+    Child child(1024, 128);
+
+    cout << child.Mother::getValue() << endl;
     cout << child.getValue() << endl;
 
     return 0;
