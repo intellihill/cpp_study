3d7bec41d2c5957b861fda4d796de085cfe57faa
intelli.hill <intelli.hill@outlook.com>
2025-09-06 14:57:55 +0900

    가상소멸자

---
+++ section/가상소멸자.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+class Base {
+public:
+    ~Base() {
+        cout << "~Base()" << endl;
+    }
+};
+
+class Derived : public Base {
+private:
+    int *m_array;
+
+public:
+    Derived(int length) {
+        m_array = new int[length];
+    }
+
+    ~Derived() {
+        cout << "~Derived()" << endl;
+        delete[] m_array;
+    }
+};
+
+int main() {
+
+    // Derived derived(5);
+
+    Derived * derived = new Derived(5);
+    Base *base = derived;
+    delete base;
+
+
+    return 0;
+}
