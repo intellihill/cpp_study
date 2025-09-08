fc2c89b2e88877eaad4d70b4ad7de47454ba7f4e
intelli.hill <intelli.hill@outlook.com>
2025-09-08 21:25:31 +0900

    func_template

---
+++ section/func_template.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "Cents.h"
+
+template<typename T>
+T getMax(T x, T y) {
+    return ( x > y ) ? x : y;
+}
+
+int main() {
+    std::cout << getMax(1, 2) << std::endl;
+    std::cout << getMax(3.14, 1.592) << std::endl;
+    std::cout << getMax(1.0f, 3.4f) << std::endl;
+    std::cout << getMax('a', 'c') << std::endl;
+    
+
+    return 0;
+}
