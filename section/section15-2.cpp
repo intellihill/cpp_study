269a5321471b3d54a4567d89044f8673a009a9c0
intelli.hill <intelli.hill@outlook.com>
2025-11-06 05:40:52 +0900

    r-value

---
+++ section/section15-2.cpp
@@ -0,0 +1,46 @@
+
+void doSomething(int &lref) {
+    cout << "L-value ref" << endl;
+}
+
+void doSomething(int &&rref) {
+    cout << "R-value ref" << endl;
+}
+
+int getResult() {
+    return 4;
+}
+
+int main() {
+    int x= 5;
+    int y= getResult();
+    const int cx= 6;
+    const int cy= getResult();
+
+    // L-value reference
+
+    int &lr1= x; // Modifiable l-values
+    // int &lr2= cx; // Non-modifiable l-values
+    // int &lr3= 5; // R-values: 참조하고있는 변수가 메모리가 있어야한다.
+
+    const int &lr4= x; // Modifiable l-values
+    const int &lr5= cx; // Non-modifiable l-values
+    const int &lr6= 5; // R-values
+
+    // R-value references
+
+    // int &&rr1= x; // Modifiable l-values
+    // int &&rr2= cx; // Non-modifiable l-values
+    int &&rr3= 5 // R-values
+
+    // const int &&rr4= x; // Modifiable l-values
+    // const int &&rr5= cx; // Non-modifiable l-values
+    const int &&rr6= 5; // R-values
+
+    // L/R-value reference parameters
+    doSomething(x);
+    doSomething(5);
+    doSomething(getResult());
+
+    return 0;
+}
